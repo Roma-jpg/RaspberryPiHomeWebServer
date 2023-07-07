c0bf25ada744b07e050ac0cb7f3dfe4ea0547fba
Romeo558 <rkoshelev2001@gmail.com>
2023-07-07 23:35:01 +0300

    well, i added something Now i have fully functional login page and i started to doing tabview (manual control of the board)

---
+++ RaspberryPiHomeWebServer/static/js/tabs.js
@@ -0,0 +1,16 @@
+function openTab(evt, tabName) {
+  var i, tabContent, tabLinks;
+
+  tabContent = document.getElementsByClassName("tab-pane");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+
+  tabLinks = document.getElementsByClassName("tab");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
